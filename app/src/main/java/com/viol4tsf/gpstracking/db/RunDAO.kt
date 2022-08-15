76de2ce99cb9e502f42095d5403638da04a6ae35
ISergienkoSF <sergienko.ivan2020@mail.ru>
2022-08-15 20:29:48 +0300

    GPSTracking App

---
+++ app/src/main/java/com/viol4tsf/gpstracking/db/RunDAO.kt
@@ -0,0 +1,40 @@
+package com.viol4tsf.gpstracking.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC") //сортировка по дате по убыванию, сначала новые записи
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC") //сортировка по времени пробежки по убыванию, сначала новые записи
+    fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC") //сортировка по калориям по убыванию, сначала новые записи
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC") //сортировка по средней скорости по убыванию, сначала новые записи
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC") //сортировка по дистанции по убыванию, сначала новые записи
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table") // сумма всего времени пробежки
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table") // сумма всего сожженных калорий
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table") // сумма всего дистанции
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table") // средняя скорость
+    fun getTotalAvgSpeed(): LiveData<Float>
+}
