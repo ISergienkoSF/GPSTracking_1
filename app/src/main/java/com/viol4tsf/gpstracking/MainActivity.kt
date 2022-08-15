540e8c0cbd7698cbf1c0d86746d8f72955da21ac
ISergienkoSF <sergienko.ivan2020@mail.ru>
2022-08-15 17:20:31 +0300

    GPSTracking App

---
+++ app/src/main/java/com/viol4tsf/gpstracking/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.viol4tsf.gpstracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
