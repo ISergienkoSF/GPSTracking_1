54b8b6d7b762feb3cc8981c1c75fb349c36d8c2d
ISergienkoSF <sergienko.ivan2020@mail.ru>
2022-08-19 18:58:47 +0300

    GPSTracking App

--- app/src/main/java/com/viol4tsf/gpstracking/db/RunningDatabase.kt
+++ app/src/main/java/com/viol4tsf/gpstracking/db/RunningDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [Run::class], version = 1)
-@TypeConverters(TypeConverters::class)
+@TypeConverters(com.viol4tsf.gpstracking.db.TypeConverters::class)
 abstract class RunningDatabase : RoomDatabase(){
     abstract fun getRunDao(): RunDao
 }
