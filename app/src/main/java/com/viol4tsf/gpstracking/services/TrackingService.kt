302258da1ec6211e4e91ce0b4eedcd352f797199
ISergienkoSF <sergienko.ivan2020@mail.ru>
2022-08-23 16:57:26 +0300

    tracking service

--- app/src/main/java/com/viol4tsf/gpstracking/services/TrackingService.kt
+++ app/src/main/java/com/viol4tsf/gpstracking/services/TrackingService.kt
@@ -7,25 +7,64 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.maps.model.LatLng
 import com.viol4tsf.gpstracking.R
 import com.viol4tsf.gpstracking.other.Constants.ACTION_PAUSE_SERVICE
 import com.viol4tsf.gpstracking.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.viol4tsf.gpstracking.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.viol4tsf.gpstracking.other.Constants.ACTION_STOP_SERVICE
+import com.viol4tsf.gpstracking.other.Constants.FASTEST_LOCATION_INTERVAL
+import com.viol4tsf.gpstracking.other.Constants.LOCATION_UPDATE_INTERVAL
 import com.viol4tsf.gpstracking.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.viol4tsf.gpstracking.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.viol4tsf.gpstracking.other.Constants.NOTIFICATION_ID
+import com.viol4tsf.gpstracking.other.TrackingUtility
 import com.viol4tsf.gpstracking.ui.MainActivity
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng> //список координат
+typealias Polylines = MutableList<Polyline> //список списков координат
 
 //фоновая служба
 class TrackingService : LifecycleService(){
 
     var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>() //координаты
+    }
+
+    //отправка начальных значений
+    private fun postInitialValues(){
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -50,8 +89,65 @@ class TrackingService : LifecycleService(){
         return super.onStartCommand(intent, flags, startId)
     }
 
+    //активация отслеживания местоположения
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if(isTracking) {
+            if (TrackingUtility.hasLocationPermissions(this)){
+                val request = LocationRequest.create().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    //коллбэк для обновления локации
+    val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if(isTracking.value!!) {
+                result?.locations?.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+    //добавление точки координат в конец списка
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                //добавление координаты на последнее место
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    //добавление пустой полилинии
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf())) //добавление новой пустой полилинии если значение 0(список пуст)
+
     //функция для фактического запуска службы
     private fun startForegroundService() {
+
+        addEmptyPolyline()
+        isTracking.postValue(true)
+
         //служба уведомлений
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
